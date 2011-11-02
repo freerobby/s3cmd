24a306e63071270c93fac0cbcd25f67b3f75086b
Robby Grossman <robby@freerobby.com>
2011-11-02 14:46:54 -0400

    Enable customization of aws credentials.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,6 @@
 # list of users that will have the s3cmd configuration 
 default[:s3cmd][:users] = [:root]
+
+# S3 credentials
+default[:s3cmd][:aws_access_key_id] = ""
+default[:s3cmd][:aws_secret_access_key] = ""
