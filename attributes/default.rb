ce8f52076683a8dee628067c25c8ccf373620921
Robby Grossman <robby@freerobby.com>
2011-11-02 13:00:41 -0400

    Fully document existing behavior.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,2 @@
-
-#list of users that will have the s3cmd configuration 
-default[:s3cmd][:users] = [:root] 
+# list of users that will have the s3cmd configuration 
+default[:s3cmd][:users] = [:root]
--- metadata.rb
+++ metadata.rb
@@ -4,3 +4,7 @@ license          "Apache 2.0"
 description      "Installs and configures s3cmd"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
+
+recipe "s3cmd", "Installs and configures s3cmd"
+
+supports "ubuntu"
