5d7d063ab528641f92163e866e380aa4b575d7f7
Robby Grossman <robby@freerobby.com>
2014-03-06 14:17:12 -0500

    Single-quote strings when not using inspection.

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,9 +2,9 @@
 default[:s3cmd][:users] = ['root']
 
 # S3 credentials
-default[:s3cmd][:encrypted_data_bag] = "aws_credentials"
-default[:s3cmd][:encrypted_data_bag_item] = "credentials" # should contain aws_access_key_id and aws_secret_access_key keys
+default[:s3cmd][:encrypted_data_bag] = 'aws_credentials'
+default[:s3cmd][:encrypted_data_bag_item] = 'credentials' # should contain aws_access_key_id and aws_secret_access_key keys
 
-default[:s3cmd][:version] = "v1.5.0-alpha3"
-default[:s3cmd][:user] = "root"
-default[:s3cmd][:install_prefix_root] = "/usr/local"
+default[:s3cmd][:version] = 'v1.5.0-alpha3'
+default[:s3cmd][:user] = 'root'
+default[:s3cmd][:install_prefix_root] = '/usr/local'
